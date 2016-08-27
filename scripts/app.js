ccdcd2baa5ca99b58f88e8a5c9addc2c89eede5b
Bill Malone <bmalone021@gmail.com>
2016-08-27 04:23:19 +0000

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-angular.module('blocJams', []); 
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+    
+        $stateProvider
+            .state('landing', { 
+                url: '', // I don't think I need '/' like the directions say since nothing else comes before this. !! 
+                templateUrl: 'templates/landing.html' // '/templates/landing'
+            })
+            .state('album', {
+                url: 'album', // !! same /album
+                templateUrl: 'templates/album.html' // !! same
+            })
+            .state('collection', {
+                url: 'collection', // !! same
+                templateUrl: 'templates/collection.html' // !! same
+            });
+    }
+
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
